0dd66131277b02c41aa537640277e11d018c7c5d
Raymond T. Hightower <rth@wisdomgroup.com>
2013-01-20 21:00:47 -0600

    Initial Commit

---
+++ UtilitySandbox/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UtilitySandbox
+//
+//  Created by RTH on 1/20/13.
+//  Copyright (c) 2013 WisdomGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainViewController *mainViewController;
+
+@end
---
+++ UtilitySandbox/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  UtilitySandbox
+//
+//  Created by RTH on 1/20/13.
+//  Copyright (c) 2013 WisdomGroup. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+- (IBAction)showInfo:(id)sender;
+
+@end
